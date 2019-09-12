b48217b3e6c03c7785d764f063c8915b116f0416
rusnasonov <rus.nasonov@gmail.com>
2019-09-12 15:05:35 +0700

    add github token to input

--- commit/entrypoint.sh
+++ commit/entrypoint.sh
@@ -4,10 +4,10 @@ git_setup ( ) {
   cat <<- EOF > $HOME/.netrc
 		machine github.com
 		login $GITHUB_ACTOR
-		password $GITHUB_TOKEN
+		password $INPUT_GITHUB_TOKEN
 		machine api.github.com
 		login $GITHUB_ACTOR
-		password $GITHUB_TOKEN
+		password $INPUT_GITHUB_TOKEN
 EOF
   chmod 600 $HOME/.netrc
 
